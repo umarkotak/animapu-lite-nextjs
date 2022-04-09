c9febf63a46dc877d1f60c0788f74b0666569288
umar.ramadhana <umar.ramadhana@gmail.com>
2022-04-09 08:50:28 +0700

    add meta and check render

--- pages/mangas/[manga_source]/[id]/read/[chapter_id].js
+++ pages/mangas/[manga_source]/[id]/read/[chapter_id].js
@@ -124,14 +124,22 @@ export default function ReadManga() {
     <div className="bg-[#d6e0ef]">
       <Head>
         <title>Animapu - Lite</title>
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Baca komik gratis tanpa iklan" />
 
-        <meta name="description" property="description" key="description" content="Tempat baca komik gratis tanpa iklan" />
-        <meta name="og:title" property="og:title" key="og:title" content="Animapu - Lite" />
-        <meta name="og:type" property="og:type" key="og:type" content="book" />
-        <meta name="og:url" property="og:url" key="og:url" content="https://animapu-lite.vercel.app/" />
-        <meta name="og:image" property="og:image" key="og:image" content="/images/cover.jpeg" />
-        <meta name="og:description" property="og:description" key="og:description" content="Tempat baca komik gratis tanpa iklan" />
+        <meta itemprop="name" content="Animapu - Lite" />
+        <meta itemprop="description" content="Baca komik gratis tanpa iklan" />
+        <meta itemprop="image" content="https://animapu-lite.vercel.app/images/cover.jpeg" />
+
+        <meta name="og:url" property="og:url" content="https://animapu-lite.vercel.app/" />
+        <meta name="og:type" property="og:type" content="website" />
+        <meta name="og:title" property="og:title" content="Animapu - Lite" />
+        <meta name="og:description" property="og:description" content="Baca komik gratis tanpa iklan" />
+        <meta name="og:image" property="og:image" content="https://animapu-lite.vercel.app/images/cover.jpeg" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Animapu - Lite" />
+        <meta name="twitter:description" content="Baca komik gratis tanpa iklan" />
+        <meta name="twitter:image" content="https://animapu-lite.vercel.app/images/cover.jpeg" />
       </Head>
 
       <div>
