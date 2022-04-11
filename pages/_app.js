c29b52c43a4759d82b2d8a0f9a0bae344cb0a106
umar.ramadhana <umar.ramadhana@gmail.com>
2022-04-11 15:31:35 +0700

    loading indicator

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,20 @@
 import '../styles/globals.css'
 import '../styles/uicons-regular-rounded.css'
 import Head from 'next/head'
+import NProgress from 'nprogress'
+import "nprogress/nprogress.css"
+import Router from 'next/router'
+
+NProgress.configure({
+  minimum: 0.3,
+  easing: 'ease',
+  speed: 800,
+  showSpinner: true,
+})
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp({ Component, pageProps }) {
   return (
