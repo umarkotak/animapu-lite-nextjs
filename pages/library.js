b122dd4d7274deae55dfd0bf1786e588881e2621
umar.ramadhana <umar.ramadhana@gmail.com>
2022-04-17 13:50:58 +0700

    library synced

--- pages/library.js
+++ pages/library.js
@@ -2,23 +2,105 @@ import { useState, useEffect } from 'react'
 
 import BottomMenuBar from "../components/BottomMenuBar"
 import MangaCard from "../components/MangaCard"
+import animapuApi from "../apis/AnimapuApi"
 
+var mangaSynced
 export default function Library() {
-  const [mangas, setMangas] = useState([
-    {id: "dummy-1", shimmer: true},
-    {id: "dummy-2", shimmer: true},
-    {id: "dummy-3", shimmer: true}
-  ])
+  const [mangas, setMangas] = useState([])
 
-  async function GetLatestManga() {
+  const [updateStatus, setUpdateStatus] = useState({
+    current: 0,
+    currentTitle: "",
+    max: 0,
+    percent: 0,
+    finished: false
+  })
+
+  function GetLatestManga() {
     var listKey = `ANIMAPU_LITE:FOLLOW:LOCAL:LIST`
     var historyArrayString = localStorage.getItem(listKey)
+    mangaSynced = false
 
     if (historyArrayString) {
       setMangas(JSON.parse(historyArrayString))
-      console.log("LIBRARY LIST", JSON.parse(historyArrayString))
+      // console.log("LIBRARY LIST", JSON.parse(historyArrayString))
     } else {
       setMangas([])
+    }
+  }
+
+  function showLatestChapter(manga) {
+    if (!manga) { return 0 }
+
+    var latestChapter = manga.latest_chapter_number
+    if (latestChapter <= 0 || !latestChapter) {
+      if (manga.chapters.length > 0) {
+        latestChapter = manga.chapters[0].number
+      }
+    }
+    if (latestChapter) {
+      return latestChapter
+    }
+    return 0
+  }
+
+  async function CheckForUpdates() {
+    if (mangas.length <= 0) { return }
+
+    var idx = 1
+    var anyUpdate = false
+    var libraryArray
+    for (const manga of mangas) {
+      if (!mangaSynced) {
+        if (!manga.local_updated_at || (manga.local_updated_at+3600) <= Math.floor(Date.now() / 1000) ) {
+          const response = await animapuApi.GetMangaDetail({
+            manga_source: manga.source,
+            manga_id: manga.source_id,
+            secondary_source_id: manga.secondary_source_id
+          })
+          const body = await response.json()
+          var mangaDetail
+          if (response.status == 200) {
+            mangaDetail = body.data
+          }
+
+          if (showLatestChapter(mangaDetail) > showLatestChapter(manga)) {
+            var listKey = `ANIMAPU_LITE:FOLLOW:LOCAL:LIST`
+            var detailKey = `ANIMAPU_LITE:FOLLOW:LOCAL:DETAIL:${manga.source}:${manga.source_id}:${manga.secondary_source_id}`
+
+            var libraryArrayString = localStorage.getItem(listKey)
+
+            if (libraryArrayString) {
+              libraryArray = JSON.parse(libraryArrayString)
+            } else {
+              libraryArray = []
+            }
+
+            libraryArray = libraryArray.filter(arrManga => !(`${arrManga.source}-${arrManga.source_id}` === `${manga.source}-${manga.source_id}`))
+
+            var tempManga = mangaDetail
+            libraryArray.unshift(tempManga)
+
+            localStorage.setItem(listKey, JSON.stringify(libraryArray))
+            localStorage.setItem(detailKey, JSON.stringify(tempManga))
+            anyUpdate = true
+          }
+        }
+      }
+
+      setUpdateStatus({
+        current: idx, currentTitle: manga.title, max: mangas.length, percent: ((idx)/mangas.length)*100, finished: false
+      })
+      idx = idx+1
+    }
+    mangaSynced = true
+
+    setUpdateStatus({
+      current: 0, currentTitle: "", max: 0, percent: 100, finished: true
+    })
+
+    if (anyUpdate) {
+      setMangas(libraryArray)
     }
   }
 
@@ -26,6 +108,11 @@ export default function Library() {
     GetLatestManga()
   // eslint-disable-next-line
   }, [])
+
+  useEffect(() => {
+    CheckForUpdates()
+  // eslint-disable-next-line
+  }, [mangas])
 
   return (
     <div className="bg-[#d6e0ef]">
@@ -37,6 +124,14 @@ export default function Library() {
 
       <div className="pt-4">
         <div className="container mx-auto max-w-[1040px]">
+          <div className={`px-4 ${updateStatus.finished ? "hidden" : "block"}`}>
+            <div className="mb-1 text-base font-medium text-white">
+              Checking for updates ({updateStatus.currentTitle}) {updateStatus.current}/{updateStatus.max}
+            </div>
+            <div className="w-full h-1.5 bg-gray-200 rounded-full dark:bg-gray-700 mb-8">
+              <div className="h-1.5 bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{width: `${updateStatus.percent}%`}}></div>
+            </div>
+          </div>
           <div className="grid grid-rows-1 grid-flow-col">
             <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4">
               {mangas.map((manga, idx) => (
