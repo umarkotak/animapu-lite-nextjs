f22c91d5513f7976604a1ce78f1fba292383e05a
umar ramadhana <umar.ramadhana@gojek.com>
2022-05-01 07:42:02 +0700

    fix css import

--- pages/_app.js
+++ pages/_app.js
@@ -39,7 +39,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="twitter:description" content="Baca komik gratis tanpa iklan" />
         <meta name="twitter:image" content="https://animapu-lite.vercel.app/images/cover.jpeg" />
 
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </Head>
 
       <Component {...pageProps} />
