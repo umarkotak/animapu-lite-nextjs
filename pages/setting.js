3b383785af16893dea189233e86d9f815174df2a
umar ramadhana <umar.ramadhana@gojek.com>
2022-07-13 04:57:12 +0700

    fix class

--- pages/setting.js
+++ pages/setting.js
@@ -148,7 +148,7 @@ export default function Setting() {
             <h2 className="text-xl mb-2">Profile</h2>
 
             <div>
-              <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Profile code</label>
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Profile code</label>
               <input
                 type="text"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
