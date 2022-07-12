78e3f4fc7e96c822a7519fceb5109e8f5b0bf1c8
umar ramadhana <umar.ramadhana@gojek.com>
2022-07-13 05:00:19 +0700

    fix quote

--- pages/setting.js
+++ pages/setting.js
@@ -154,7 +154,9 @@ export default function Setting() {
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="secret_jhon_special"
               />
-              <small>your secret prase to be used as an identity if you want to save your read history online. leave it blank if you don't want to save your history online</small>
+              <small>
+                your secret prase to be used as an identity if you want to save your read history online. leave it blank if you do not want to save your history online.
+              </small>
             </div>
           </div>
 
