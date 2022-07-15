148e374c826b97795480ce77ed53caf6f7660af4
umar ramadhana <umar.ramadhana@gojek.com>
2022-07-15 09:56:32 +0700

    fix tabindex

--- components/ChangeSourceModal.js
+++ components/ChangeSourceModal.js
@@ -89,7 +89,7 @@ export default function ChangeSourceModal(props) {
       </div>
       {
         show &&
-        <div id="" tabindex="-1" className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center flex block " aria-modal="true" role="dialog">
+        <div tabIndex="-1" className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center flex block " aria-modal="true" role="dialog">
           <div className="relative p-4 w-full max-w-md h-full md:h-auto">
             <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
                 <button type="button" className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" onClick={reveal}>
