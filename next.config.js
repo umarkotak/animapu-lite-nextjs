7f6faed12f9a9edd83e9c02ba90bd84be602fc77
umar <umarkotak@gmail.com>
2023-07-02 16:50:01 +0700

    MAIN NEW

--- next.config.js
+++ next.config.js
@@ -5,11 +5,19 @@
 
 // module.exports = nextConfig
 
-const withPWA = require('next-pwa')
+// const withPWA = require('next-pwa')
+
+// module.exports = withPWA({
+//     pwa: {
+//         dest: 'public',
+//         disable: process.env.NODE_ENV === 'development'
+//     }
+// })
+
+const withPWA = require('next-pwa')({
+    dest: 'public'
+})
 
 module.exports = withPWA({
-    pwa: {
-        dest: 'public',
-        disable: process.env.NODE_ENV === 'development'
-    }
+// next.js config
 })
