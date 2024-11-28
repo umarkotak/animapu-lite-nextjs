fb8cadff953451d26e44865d029ad2d9b11baa6b
umar.ramadhana <umar.ramadhana@everywhere.id>
2024-11-29 06:17:52 +0700

    update-host

--- apis/AnimapuApi.js
+++ apis/AnimapuApi.js
@@ -1,9 +1,9 @@
 class AnimapuApi {
   constructor() {
     if (typeof(window) !== "undefined" && window.location.protocol === "https:") {
-      this.AnimapuApiHost = "https://api.shadow-animapu-2.site"
+      this.AnimapuApiHost = "https://animapu-api.cloudflare-avatar-id-1.site"
     } else {
-      this.AnimapuApiHost = "https://api.shadow-animapu-2.site"
+      this.AnimapuApiHost = "https://animapu-api.cloudflare-avatar-id-1.site"
       // this.AnimapuApiHost = "http://localhost:6001"
     }
 
