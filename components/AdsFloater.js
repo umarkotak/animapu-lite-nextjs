46078fc0d59aeca8535220a033a738e807984435
umar.ramadhana <umar.ramadhana@everywhere.id>
2025-01-02 12:52:55 +0700

    add tracker

--- components/AdsFloater.js
+++ components/AdsFloater.js
@@ -6,21 +6,25 @@ import animapuApi from "../apis/AnimapuApi"
 import Manga from "../models/Manga"
 import { BookIcon, Eye, EyeIcon, Heart, HeartIcon, PlayIcon, Share2Icon, StarIcon, XIcon } from 'lucide-react'
 import { toast } from 'react-toastify'
+import * as Cronitor from "@cronitorio/cronitor-rum"
 
 export default function AdsFloater() {
   const [prayerTimes, setPrayerTimes] = useState({})
   const [adsList, setAdsList] = useState([
     {
+      "codename": "kentang-mustofa",
       "link": "https://tokopedia.link/PDkVcWWVOPb",
       "image": "https://images.tokopedia.net/img/cache/900/VqbcmM/2024/2/26/006c2c90-bf67-4195-bde6-de58107390c4.jpg",
       "name": "Kentang Mustofa Kacang Teri toples 1 ltr - Kacang Teri",
     },
     {
+      "codename": "slice-beef",
       "link": "https://tokopedia.link/3Kbn0gxWOPb",
       "image": "https://images.tokopedia.net/img/cache/900/VqbcmM/2024/6/12/3e3b7e1e-1c50-4464-bc45-1c7fe63dca15.jpg",
       "name": "Hijrahfood Slice Beef US Shortplate | Daging Lapis Iris Shabu / Sukiyaki / Yakiniku / Yoshinoya - 250 gram",
     },
     {
+      "codename": "celana-pendek",
       "link": "https://tokopedia.link/6VOnaKzWOPb",
       "image": "https://images.tokopedia.net/img/cache/900/VqbcmM/2024/10/9/b65129d5-825f-4ac2-a581-04650a2af9fd.jpg",
       "name": "promo 4 pcs celana pendek pria wanita dewasa santai baby terry saku resleting jumbo - 3PCS, XL 60-70 KG",
@@ -41,6 +45,10 @@ export default function AdsFloater() {
 
     fetchPrayerTimes();
   }, [])
+
+  async function trackClick(codename) {
+    Cronitor.track(`ADS_CLICK_${codename}`)
+  }
 
   return(<>
     <div className='fixed w-[270px] top-[60px] z-0 left-1/2 hidden xl:block text-white' style={{transform: `translate(-660px, 0%)`}}>
@@ -91,8 +99,8 @@ export default function AdsFloater() {
           Ads / Iklan
         </div>
         {adsList.map((oneAds) => (
-          <div className='w-full overflow-hidden rounded-lg relative border border-white' key={oneAds.link}>
-            <Link href={oneAds.link} target="_blank">
+          <div className='w-full overflow-hidden rounded-lg relative border border-white' key={oneAds.codename}>
+            <Link href={oneAds.link} target="_blank" onClick={()=>trackClick(oneAds.codename)}>
               <img className='cursor-pointer hover:scale-105 transition' src={oneAds.image} />
               <div className='bottom-0 w-full bg-black bg-opacity-70 backdrop-blur-sm absolute p-2 text-sm line-clamp-3 rounded-b-lg'>
                 {oneAds.name}
